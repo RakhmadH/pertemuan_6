d932c955a24f0594e9ce953a5c4dbb518c52fb47
RakhmadH <rakhmad.hernan.syah@mail.ugm.ac.id>
2022-12-01 15:38:45 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -96,3 +96,4 @@ Auth::routes([
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('gallery', 'App\Http\Controllers\GalleryController');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+URL::forceScheme('https');
